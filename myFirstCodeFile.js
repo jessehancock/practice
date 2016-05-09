6fc08e850d70fe936a4f7742cc1e53a2b394a12e
jessehancock <jessehancock1@gmail.com>
2016-05-09 14:36:59 -0600

    this was the second change

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -1,5 +1,9 @@
 var dm11 = "Started today";
+var feeling = "excited";
+var lookingForward = "hell yes"
 
 var journal = [];
 
 journal.push(dm11);
+journal.push(feeling);
+journal.push(lookingForward);
