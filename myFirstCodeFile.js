2b80651c320989c08583e3db5fefaacba7a094da
jessehancock <jessehancock1@gmail.com>
2016-05-09 14:39:59 -0600

    get three

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -1,9 +1,11 @@
 var dm11 = "Started today";
 var feeling = "excited";
-var lookingForward = "hell yes"
+var lookingForward = "hell yes";
+var feelingNow = "oh yes";
 
 var journal = [];
 
 journal.push(dm11);
 journal.push(feeling);
 journal.push(lookingForward);
+journal.push(feelingNow);
